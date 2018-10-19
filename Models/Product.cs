046c3c4a028eb5844fcb66560bc7f67d6c1bb5a5
anhquoc4062 <anhquoc4062@gmail.com>
2018-10-19 17:36:08 +0700

    abc

--- Models/Product.cs
+++ Models/Product.cs
@@ -12,25 +12,27 @@ namespace WebApplication1.Models
     {
         [BsonId]
         [DisplayName("Mã sản phẩm")]
-        public ObjectId product_id;
+        public ObjectId product_id { get; set; }
         [BsonElement]
         [DisplayName("Tên sản phẩm")]
-        public string product_name;
+        public string product_name { get; set; }
         [BsonElement]
         [DisplayName("Đơn giá")]
-        public int product_price;
+        public int product_price { get; set; }
         [BsonElement]
         [DisplayName("Mã loại")]
-        public ObjectId category_id;
+        public ObjectId category_id { get; set; }
         [BsonElement]
         [DisplayName("Hình ảnh")]
-        public string product_img;
+        public string product_img { get; set; }
         [BsonElement]
         [DisplayName("Mô tả")]
-        public string product_info;
+        public string product_info { get; set; }
         [BsonElement]
         [DisplayName("Ngày tạo")]
-        public string product_date;
+        public string product_date { get; set; }
+        
+        public string category_id_string { get; set; }
 
     }
 }
